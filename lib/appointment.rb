d15aac61fae82b6f4cfb86269dc0b8ff2ccc4db4
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-28 18:34:06 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -5,14 +5,12 @@ class Appointment
   attr_accessor :patient, :doctor, :date
 
   def initialize(patient, doctor, date)
-    @patient = patient
-    @doctor = doctor
     @date = date
     @@all << self
   end
 
   def self.all
     @@all
-  end 
+  end
 
 end
